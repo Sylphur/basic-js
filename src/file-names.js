99923dd8c8f8af33091282f0a256e7f598e11d44
Evgeny Redchenko <neosylphur@gmail.com>
2022-10-18 02:30:02 +0300

    feat: add filenames, get-email, mac-adress solutions

--- src/file-names.js
+++ src/file-names.js
@@ -15,9 +15,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  let res = [];
+  let repeatCounter = 0;
+  for (let i = 0; i < names.length; i++) {
+    if (res.includes(names[i])) {
+      for (let j = 0; j < i; j++) {
+        if (names[j] === names[i]) repeatCounter++;
+      }
+      if (repeatCounter === 0) res.push(`${names[i]}(${repeatCounter + 1})`);
+      else {
+        res.push(`${names[i]}(${repeatCounter})`);
+        repeatCounter = 0; }
+    }
+    // else if (repeatCounter > 0) {
+    //   res.push(`${name}(${repeatCounter})`);
+    //   repeatCounter = 0;
+    // }
+    else {
+      res.push(names[i]);
+    }
+  }
+  return res;
 }
 
 module.exports = {
--- src/mac-address.js
+++ src/mac-address.js
@@ -14,9 +14,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function isMAC48Address(n) {
+  let canContain = ['-' ,'A', 'B', 'C', 'D', 'E', 'F', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  let isAdress = true;
+  for (let char of n) {
+    if (canContain.indexOf(char) < 0) isAdress = false;
+  }
+  return isAdress;
+  //let digits = n.split('-');
+  // for (let digit of digits) {
+  //   let match = digit.con
+  //   if (!parseInt(digit, 16)) return false;
+  // }
+  // return true;
+
+
+  // digits.array.forEach(element => {
+  //   for (let i = 0; i < element.length; i++) {
+  //     if (!canContain.indexOf(element[i])) isAdress = false;
+  //   }
+  // });
 }
 module.exports = {
   isMAC48Address
