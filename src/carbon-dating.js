9b7a3c93c99f3153be3b59d83088efc655753eb8
Evgeny Redchenko <neosylphur@gmail.com>
2022-10-17 19:34:30 +0300

    add carbon and season solutions

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,17 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof(sampleActivity) !== 'string') return false;
+
+  let sampleActivityNumber = Number(sampleActivity);
+  const k = 0.693 / HALF_LIFE_PERIOD;
+  if (!sampleActivityNumber ||
+    isNaN(sampleActivityNumber) ||
+    sampleActivityNumber > 15 ||
+    sampleActivityNumber < 0) return false;
+  
+    return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivityNumber) / k)
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if (!date) return 'Unable to determine the time of year!';
+  if (Object.prototype.toString.call(date) !== '[object Date]') throw new Error('Invalid date!');
+  try {
+    date.getTime();  // пытаемся взять таймстамп. Можно еще проверять isNaN так как не фейковая дата может создаваться из NaN. Будет надпись invalid date 
+                      // однако все методы Date работать будут. Инфа с хабра
+  }
+  catch {
+    throw new Error('Invalid date!');
+  }
+  
+
+  let month = date.getMonth();
+  switch (month) {
+    case 11:
+    case 0:
+    case 1: return 'winter';
+    case 2:
+    case 3:
+    case 4: return 'spring';
+    case 5:
+    case 6:
+    case 7: return 'summer';
+    case 8:
+    case 9:
+    case 10: return 'fall';
+  }
+
 }
 
 module.exports = {
