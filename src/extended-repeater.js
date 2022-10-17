3d8fce72e43b55ebd0d474e607be17f6b7c90976
Evgeny Redchenko <neosylphur@gmail.com>
2022-10-17 22:51:22 +0300

    feat: add repeater and recursive-depth solutions. Repeater can be recursive?

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,10 +15,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let string = String(str);
+  let add = options.hasOwnProperty('addition') ? String(options.addition) : '';
+  let sep = options.hasOwnProperty('separator') ? String(options.separator) : '+';
+  let addSep = options.hasOwnProperty('additionSeparator') ? String(options.additionSeparator) : '|';
+  let times = options.hasOwnProperty('repeatTimes') ? options.repeatTimes : 1;
+  let addTimes = options.hasOwnProperty('additionRepeatTimes') ? options.additionRepeatTimes : 1;
+  let oneRepeat = [];
+  let res = [];
+
+  oneRepeat.push(string);
+  for (let i = 0; i < addTimes; i++) {
+    if (i == addTimes - 1) {
+      oneRepeat.push(add)
+    }
+    else {
+      oneRepeat.push(add + addSep);
+    }
+  }
+  for (let i = 0; i < times; i++) {
+    if (i == times - 1) {
+      res.push(oneRepeat.join(''));
+    }
+    else {
+      res.push(oneRepeat.join('') + sep);
+    }
+  }
+  return res.join('');
 }
+
+
 
 module.exports = {
   repeater
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,9 +13,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth( arr ) {
+    let maxDepth = 0;
+    for (let key of arr) {
+      let depth = 0;
+      if (Array.isArray(key)) {
+        depth = this.calculateDepth(key);
+      }
+      if (depth > maxDepth) maxDepth = depth;
+    }
+    return maxDepth + 1;
   }
 }
 
