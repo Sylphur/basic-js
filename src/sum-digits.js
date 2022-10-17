68ee79adb981a4f63d9e37f6574234ccd22ddff3
Evgeny Redchenko <neosylphur@gmail.com>
2022-10-18 02:55:31 +0300

    feat: add sort-by-height and sum-digits solutions

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,9 +12,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+  let sum = getSum(n);
+  while(sum.toString().length > 1) {
+    sum = getSum(sum);
+  }
+  return sum;
+
+  function getSum(n) {
+    let str = n.toString();
+    let res = 0;
+    for (let digit of str) {
+      res += parseInt(digit);
+    }
+    return + res;
+  }
 }
 
 module.exports = {
